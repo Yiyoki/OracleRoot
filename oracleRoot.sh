b1b52bf86d94e7bbb05a4690c2c9c7f605bb4d2e
Yiyoki <37234816+Yiyoki@users.noreply.github.com>
2022-06-09 20:14:47 +0800

    Delete oracleRoot.sh

--- oracleRoot.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-passd=$1
-sudo -i
-
-echo root:${passd}|sudo chpasswd root
-sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo service sshd restart
