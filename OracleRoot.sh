c33bea8759142b6e7480d2167ea7d7fc53651c2b
Yiyoki <37234816+Yiyoki@users.noreply.github.com>
2022-06-09 20:15:14 +0800

    Add files via upload

---
+++ OracleRoot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+passwd=$1
+
+echo root:${passwd} | sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+
+sudo service sshd restart
+
